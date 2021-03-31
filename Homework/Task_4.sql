1eb48725c96a728661e4310f344acb4ecd64e210
Olha Vyzhevska <42007011+Olvyz@users.noreply.github.com>
2021-03-31 10:16:17 +0200

    Update Task_4.sql

--- Homework/Task_4.sql
+++ Homework/Task_4.sql
@@ -1,21 +1,21 @@
-select * from employees where length(first_name) > 10;
-select * from employees where mod(salary, 1000) = 0;
-select phone_number, RPAD(phone_number, 3) from employees where phone_number like('___.___.____');
-select phone_number, substr(phone_number, 1, 3) from employees where phone_number like('___.___.____');
-select first_name from employees where length(first_name) > 5 and INSTR(first_name, 'm') = length(first_name);
-select first_name from employees where length(first_name) > 5 and substr(first_name, -1) = 'm';
-select NEXT_DAY(SYSDATE, 5) from dual;
-select * from employees where months_between(sysdate, hire_date) > 150;
-select phone_number, replace(phone_number, '.', '-') from employees;
-select UPPER(first_name), lower(email), INITCAP(job_id) from employees;
-select concat(first_name, salary) from employees;
-select hire_date, round(hire_date,'mm'), trunc(hire_date, 'yyyy') from employees;
-select rpad(first_name, 10, '$'), lpad(last_name, 15, '!') from employees;
-select first_name, instr(first_name, 'a', 1, 2) from employees;
-select '!!!HELLO!! MY FRIEND!!!!!!!!', trim (both '!' from '!!!HELLO!! MY FRIEND!!!!!!!!') from dual;
-select salary, round(salary*3.1415), trunc(salary*3.1415, -3)/1000 from employees;
-select hire_date, add_months(hire_date, 6), last_day(hire_date) from employees;
-select sysdate from dual;
+SELECT * FROM employees WHERE length(first_name) > 10;
+SELECT * FROM employees WHERE mod(salary, 1000) = 0;
+SELECT phone_number, RPAD(phone_number, 3) FROM employees WHERE phone_number LIKE('___.___.____');
+SELECT phone_number, substr(phone_number, 1, 3) FROM employees WHERE phone_number LIKE('___.___.____');
+SELECT first_name FROM employees WHERE length(first_name) > 5 AND INSTR(first_name, 'm') = length(first_name);
+SELECT first_name FROM employees WHERE length(first_name) > 5 AND substr(first_name, -1) = 'm';
+SELECT NEXT_DAY(SYSDATE, 5) FROM dual;
+SELECT * FROM employees WHERE months_between(sysdate, hire_date) > 150;
+SELECT phone_number, replace(phone_number, '.', '-') FROM employees;
+SELECT UPPER(first_name), lower(email), INITCAP(job_id) FROM employees;
+SELECT concat(first_name, salary) FROM employees;
+SELECT hire_date, round(hire_date,'mm'), trunc(hire_date, 'yyyy') FROM employees;
+SELECT rpad(first_name, 10, '$'), lpad(last_name, 15, '!') FROM employees;
+SELECT first_name, instr(first_name, 'a', 1, 2) FROM employees;
+SELECT '!!!HELLO!! MY FRIEND!!!!!!!!', trim (both '!' FROM '!!!HELLO!! MY FRIEND!!!!!!!!') FROM dual;
+SELECT salary, round(salary*3.1415), trunc(salary*3.1415, -3)/1000 FROM employees;
+SELECT hire_date, add_months(hire_date, 6), last_day(hire_date) FROM employees;
+SELECT sysdate FROM dual;
 
 
 
