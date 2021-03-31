2e2c0d7516c4391cf4a8125206a2244f42d6ca4e
Olha Vyzhevska <42007011+Olvyz@users.noreply.github.com>
2021-03-31 09:56:07 +0200

    Update Task_2.sql

--- Homework/Task_2.sql
+++ Homework/Task_2.sql
@@ -1,10 +1,10 @@
-select * from REGIONS;
-select first_name, department_id, salary, last_name from employees;
-select employee_id, email, (hire_date - 7) "One week before hire date" from employees;
-select first_name || '(' || job_id || ')' as "our_employees" from employees;
-select DISTINCT first_name from employees;
-select job_title, 'min = ' || min_salary || ', max = ' || max_salary as info, max_salary as max, max_salary*2-2000 as new_salary from jobs;
-select 'Peter''s dog is very clever' from dual;
-select q'<Peter's dog is very clever>' from dual;
-select 365.25*100*24*60 from dual;
+SELECT * FROM REGIONS;
+SELECT first_name, department_id, salary, last_name FROM employees;
+SELECT employee_id, email, (hire_date - 7) "One week before hire date" FROM employees;
+SELECT first_name || '(' || job_id || ')' AS "our_employees" FROM employees;
+SELECT DISTINCT first_name FROM employees;
+SELECT job_title, 'min = ' || min_salary || ', max = ' || max_salary AS info, max_salary AS max, max_salary*2-2000 AS new_salary FROM jobs;
+SELECT 'Peter''s dog is very clever' FROM dual;
+SELECT q'<Peter's dog IS very clever>' FROM dual;
+SELECT 365.25*100*24*60 FROM dual;
 
